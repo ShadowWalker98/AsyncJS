5089009f79433415a283fa8baf1f67e3b5a150f0
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-24 21:52:31 +0530

    added reject functionality

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-function slowEcho(message) {
+function slowEcho(message, delay) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(message);
-        }, 2000);
-    })
+            let random = Math.floor(Math.random() * 10);
+            if( random % 2 == 0){
+                resolve(message);
+            } else {
+                let err = new Error('Something odd happened');
+                reject(err);
+            }
+        }, delay);
+
+    });
 }
+
+document.getElementById('btn')
+.addEventListener('click', () => {
+    slowEcho("Hello World!", 1000)
+    .then((a) => {
+        console.log(a);
+    }).catch((e) => {
+        console.log("An error occurred");
+        console.log(e)
+    });
+});
